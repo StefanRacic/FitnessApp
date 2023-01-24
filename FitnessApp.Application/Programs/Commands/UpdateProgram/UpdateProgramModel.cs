797dbf39e824c8d73f44cb3b6f2d1989dd2a446b
StefanRacic <86464790+racicstefan@users.noreply.github.com>
2023-01-24 14:15:01 +0100

    Add data validation for program models

--- FitnessApp.Application/Programs/Commands/UpdateProgram/UpdateProgramModel.cs
+++ FitnessApp.Application/Programs/Commands/UpdateProgram/UpdateProgramModel.cs
@@ -1,8 +1,12 @@
-﻿namespace FitnessApp.Application.Programs.Commands.UpdateProgram
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessApp.Application.Programs.Commands.UpdateProgram
 {
     public class UpdateProgramModel
     {
+        [Required(AllowEmptyStrings = false)]
         public string Name { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string Description { get; set; }
     }
 }
